e7381218064e74a7b0b9863f0b84d5ba83c1758a
Nathan Hall <nathanhall@MacBookPro.lan>
2026-02-09 23:25:51 -0500

    Fix TypeScript error in CanvassMap click handler

--- app/(canvass-app)/canvass/components/CanvassMap.tsx
+++ app/(canvass-app)/canvass/components/CanvassMap.tsx
@@ -71,8 +71,8 @@ export default function CanvassMap({ pins, currentPosition, onMapClick, onPinCli
     })
 
     // Add click listener
-    mapInstanceRef.current.addListener('click', (e: google.maps.MapMouseEvent) => {
-      if (e.latLng) {
+    mapInstanceRef.current.addListener('click', (e?: google.maps.MapMouseEvent) => {
+      if (e?.latLng) {
         onMapClick(e.latLng.lat(), e.latLng.lng())
       }
     })
